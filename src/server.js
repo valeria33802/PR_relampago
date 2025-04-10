3bb467837b9d3ad2fc50245833bc5da53bd7d37d
andrey <andrey@email.com>
2025-04-10 10:29:57 -0600

    fetch en busquedaclientes.html

--- src/server.js
+++ src/server.js
@@ -18,49 +18,62 @@ app.use(express.static(join(dir_name, "public")));
 
 // Ruta para servir el archivo index.html
 app.get("/", async (req, res) => {
-    try {
-        const connect = await openSQL()
-       // console.log(connect)
-        res.sendFile(join(dir_name, "public", "index.html")); // Servir index.html estático
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al servir el archivo.");
-    }
+  try {
+    const connect = await openSQL();
+    // console.log(connect)
+    res.sendFile(join(dir_name, "public", "index.html")); // Servir index.html estático
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al servir el archivo.");
+  }
 });
 
-app.get("/login", async (req, res) => {
-    try {
+// función para consultar las ruta de los archivos  pdf en la base de datos
+app.get("/rutas-pdf", async (req, res) => {
+  try {
+    const connect = await openSQL();
+    // console.log(connect)
+    const result = await connect.request().query("SELECT * FROM **********");
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al servir el archivo.");
+  }
+});
 
-        const connect = await openSQL()
-       //console.log(connect)
-        res.sendFile(join(dir_name, "public", "login.html")); // Servir index.html estático
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al servir el archivo.");
-    }
+
+app.get("/login", async (req, res) => {
+  try {
+    const connect = await openSQL();
+    //console.log(connect)
+    res.sendFile(join(dir_name, "public", "login.html")); // Servir index.html estático
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al servir el archivo.");
+  }
 });
 
 app.post("/iniciar_sesion", async (req, res) => {
-    try {
-        res.sendFile(join(dir_name, "public", "index.html")); // Servir index.html estático
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al servir el archivo.");
-    }
+  try {
+    res.sendFile(join(dir_name, "public", "index.html")); // Servir index.html estático
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al servir el archivo.");
+  }
 });
 
 app.get("/admin", async (req, res) => {
-    try {
-        res.sendFile(join(dir_name, "public", "admin.html")); // Servir index.html estático
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al servir el archivo.");
-    }
+  try {
+    res.sendFile(join(dir_name, "public", "admin.html")); // Servir index.html estático
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error al servir el archivo.");
+  }
 });
 
 // Iniciar el servidor
 app.listen(process.env.PORT || 3000, () => {
-    console.log(
-        `Server is listening on port http://localhost:${process.env.PORT || 3000}`
-    );
+  console.log(
+    `Server is listening on port http://localhost:${process.env.PORT || 3000}`
+  );
 });
